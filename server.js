3a4257b45133629c07da732edacff4d6f38a9b7f
Manny <coachmannytw@gmail.com>
2018-03-28 11:04:22 -0600

    gql + express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql');
+
+
+const app = express();
+
+app.use('graphql', expressGraphQL({
+    graphiql: true
+}));
+
+app.listen(5000, () => {
+    console.log('I am listening');
+});
